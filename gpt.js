637934465e406c71052bb3393efcf9c5aa4edb56
JHikyu <therosti@gmail.com>
2023-06-09 20:33:35 +0200

    Backlog

--- gpt.js
+++ gpt.js
@@ -6,6 +6,8 @@ const https = require('https');
 const readline = require('readline');
 const colors = require('colors');
 
+const totalBacklog = 5;
+
 //* Token functions 
 const tokenFileExists = () => {
   try {
@@ -35,14 +37,14 @@ async function getToken() {
       output: process.stdout
     });
 
-    rl.question('OpenAi Token: ', (message) => {
+    console.log('Please enter your OpenAi Token');
+    rl.question('> ', (message) => {
       saveTokenToFile(message);
       rl.close();
 
       resolve(message);
     });
   });
-
 }
 
 //* Openai
@@ -58,12 +60,19 @@ const options = token => {
   }
 }
 
+const allMessages = [];
+
+//* Main
 (async () => {
   console.log(colors.gray('Thanks for using @hikyu/cli-gpt'));
 
   // Get token
   let token = readTokenFromFile();
-  if(!token) token = await getToken();
+  if(!token) {
+    token = await getToken();
+  }
+  // await sleep(1000);
+  console.clear();
   
   const apiOptions = options(token);
 
@@ -76,13 +85,26 @@ function userInput(apiOptions) {
     input: process.stdin,
     output: process.stdout
   });
-  rl.question('👤 ', (message) => {
+
+  process.stdout.write('👤 ');
+  let message = '';
+  rl.on('line', (line) => {
+    if (line.trim() === '') rl.close();
+    else message += line + '\n';
+  });
+
+
+  rl.on('close', () => {
+    if(message.length <= 0) return;
+
+    allMessages.push({ role: 'user', content: message });
+    allMessages.splice(0, allMessages.length - totalBacklog);
 
     process.stdout.write('🤖 ');
 
-    sendMessage(apiOptions, message, (res) => {
+    sendMessage(apiOptions, (res) => {
       if(!res) {
-        console.log('\n');
+        console.log('\n\n');
         userInput(apiOptions);
         return;
       }
@@ -90,12 +112,16 @@ function userInput(apiOptions) {
       if(!res.choices[0].delta.content) return;
       process.stdout.write(res.choices[0].delta.content);
     });
+  });
 
-    rl.close();
+  rl.on('SIGINT', () => {
+    rl.close(); // Close the readline interface
+    console.clear();
+    process.exit(); // Exit the process
   });
 }
 
-function sendMessage(apiOptions, message, callback) {
+function sendMessage(apiOptions, callback) {
   let req = https.request(apiOptions, (res) => {
     res.on('data', (chunk) => {
       const obj = chunk.toString().replace(/data: /g, '').trimRight();
@@ -120,11 +146,14 @@ function sendMessage(apiOptions, message, callback) {
     model: "gpt-3.5-turbo",
     stream: true,
     max_tokens: 100,
-    messages: [
-      { role: 'user', content: message }
-    ]
+    messages: allMessages
   });
   
   req.write(data);
   req.end();
 }
+
+process.on('SIGINT', () => {
+  console.clear();
+  process.exit(); // exit the process
+});
